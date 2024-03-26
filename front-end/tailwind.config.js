52b060f449278b42bbc4dccaaa8ffdd5077b14bd
MasterTraits <emmanuelfabella606@gmail.com>
2024-03-26 22:02:28 +0800

    Added Navbar and Layout

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ 
+  // "./src/App.css",
+  // "./src/App.jsx",
+  "./src/index.css",
+  // "./src/main.jsx",    
+  "./entry.html" ],
+  theme: {
+
+    extend: {},
+  },
+  plugins: [],
+}
+
