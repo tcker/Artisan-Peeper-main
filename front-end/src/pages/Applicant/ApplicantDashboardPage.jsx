5c9b8b4df74d2ebe861aa013934d8d398ca1f86f
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-03-28 21:53:31 +0800

    Create a directory designated for the applicant

---
+++ front-end/src/pages/Applicant/ApplicantDashboardPage.jsx
@@ -0,0 +1,10 @@
+
+function ApplicantDashboardPage() {
+  return (
+    <div>
+      <h1>You are now logged in! Applicant</h1>
+    </div>
+  )
+}
+
+export default ApplicantDashboardPage
---
+++ front-end/src/pages/Applicant/ApplicantProfilePage.jsx
@@ -0,0 +1,11 @@
+
+
+function ApplicantProfilePage() {
+  return (
+    <div>
+      <h1>Applicant Profile Page</h1>
+    </div>
+  )
+}
+
+export default ApplicantProfilePage
