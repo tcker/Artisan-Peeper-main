c61ac6339eae5ec026b8b743e7e5e5d615141d80
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-03-28 21:49:46 +0800

    Import JSX and authenticate access of admin and applicant

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 import {
   Route,
   createBrowserRouter,
@@ -8,14 +8,52 @@ import {
 
 import LoginPage from "./pages/LoginPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import MainLayout from "./layout/MainLayout.jsx";
+
+// Applicant Side Import
+import ApplicantDashboardPage from "./pages/Applicant/ApplicantDashboardPage.jsx";
+import ApplicantProfilePage from "./pages/Applicant/ApplicantProfilePage.jsx";
+
+// Admin Side Import
+import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
 
 function App() {
+  
+const [isAuthenticated, setIsAuthenticated] = useState(true)
+const [isAdmin, setIsAdmin] = useState(false)
 
   const router = createBrowserRouter (
     createRoutesFromElements (
       <>
+        {/* Authentication */}
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/register" element={<SignUpPage/>}/>
+
+        {/* Applicant Authentication */}
+        {isAuthenticated && !isAdmin ? (
+          <>
+            <Route path='/' element={<MainLayout/>}>
+              <Route path='/dashboard' element={<ApplicantDashboardPage/>}/>
+              <Route path='/profile' element={<ApplicantProfilePage/>}/>
+            </Route>
+          </>
+        ) : (
+          <Route path='/' render={() => <LoginPage/>}/>
+        )}
+
+        {/* Admin Authentication */}
+        {isAuthenticated && isAdmin ? (
+          <>
+            <Route path='/' element={<MainLayout/>}/>
+            <Route path='/dashboard' element={<AdminDashboard/>}/>
+          </>
+        ) : (
+          <Route path='/login' render={() => <LoginPage/>}/>
+        )}
+
+        {/* 404 Error Page */}
+        <Route path='*' element={<NotFoundPage/>}/>
       </>
     )
   )
