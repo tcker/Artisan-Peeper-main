75e4e6513bdbc919926e41c2ff4f06b064892253
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-03-31 00:18:01 +0800

    feat: auth if user is login as user

--- backend/auth.js
+++ backend/auth.js
@@ -11,3 +11,32 @@ module.exports.createAccessToken = (user) => {
     expiresIn: '30d'
   })
 }
+
+module.exports.verify = (req, res, next) => {
+  console.log(req.headers.authorization);
+
+  let token = req.headers.authorization;
+
+  console.log(token);
+
+  if(typeof token == "undefined"){
+    return res.send({ auth: "Failed. No Token!"});
+  } else {
+    token = token.slice(7, token.length);
+    console.log(token);
+
+    jwt.verify(token, secret, function(err, decodedToken){
+      if(err){
+        return res.send({
+          auth: "Failed",
+          message: err.message
+        })
+      } else {
+        console.log(req.user)
+        console.log(decodedToken)
+        req.user = decodedToken
+        next()
+      }
+    })
+  }
+}
