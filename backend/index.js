9f270b060c9de0ac51d52d857e21e6ef94db7efc
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-03-30 23:53:13 +0800

    setup mongodb to index.js

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const port = 4000
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+mongoose.connect("mongodb://localhost:27017")
+mongoose.connection.once("open", () => console.log("Now connected to MongoDB Atlas"))
+
+if(require.main == module){
+  app.listen(port, () => console.log(`API is now online on port ${port}`))
+}
+
