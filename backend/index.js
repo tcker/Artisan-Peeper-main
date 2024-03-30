b54a58db13c0d5d34df9eea990da5e6792fe5014
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-03-30 23:56:24 +0800

    feat: connect index.js to userRoute

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,8 @@ app.use(cors())
 mongoose.connect("mongodb://localhost:27017")
 mongoose.connection.once("open", () => console.log("Now connected to MongoDB Atlas"))
 
+app.use("/users", require("./routes/userRoute"))
+
 if(require.main == module){
   app.listen(port, () => console.log(`API is now online on port ${port}`))
 }
