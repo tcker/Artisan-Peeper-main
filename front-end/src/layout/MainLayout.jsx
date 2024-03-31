893d249ec855776722dd1ad96c7555a2b2dda850
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-03-31 17:51:54 +0800

    Add toastify

--- front-end/src/layout/MainLayout.jsx
+++ front-end/src/layout/MainLayout.jsx
@@ -1,11 +1,15 @@
 import {Outlet} from 'react-router-dom'
 import Navbar from '@/components/Navbar.jsx'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
+
 
 function MainLayout({isAssessmentOpen, isAdmin}) {
   return (
     <>
       <Navbar isAssessmentOpen={isAssessmentOpen} isAdmin={isAdmin}/>
       <Outlet/>
+      <ToastContainer/>
     </>
   )
 }
