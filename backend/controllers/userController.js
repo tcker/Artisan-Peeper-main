9bd1ee8594aa55e270df7f1b0ead53784d1aa3e2
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-04-03 14:28:21 +0800

    feat: create token functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,58 +1,47 @@
-const asyncHandler = require("express-async-handler")
-const User = require("../models/userModel")
-const bcrypt = require("bcrypt")
-const auth = require("../auth")
+import asyncHandler from "express-async-handler"
+import User from "../models/userModel.js"
+import bcrypt from "bcryptjs"
+import createToken from "../createToken.js"
 
 const registerUser = asyncHandler( async(req, res) => {
   const { firstName, lastName, email, password, isAdmin } = req.body
-  if(!email && !password){
+  if(!firstName || !lastName || !email || !password){
     res.send(400).json("Please enter all fields")
   }
 
   const userExists = await User.findOne({email})
   if(userExists){
-    res.status(400)
-    alert("Email already Exists")
+    res.status(400).json("Email already Exists")
   }
 
   const salt = await bcrypt.genSalt(10)
   const hashedPassword = await bcrypt.hash(password, salt)
-  const user = await User.create({
-    firstName, lastName, email, isAdmin, password: hashedPassword
+
+  const newUser = new User({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+    isAdmin: isAdmin
   })
 
-  if(user){
-    res.status(201)
-    res.json({
-      _id: user.id,
-      email: user.email
+  try {
+    await newUser.save();
+    createToken(res, newUser._id)
+
+    res.status(201).json({
+      _id: newUser._id,
+      firstName: newUser.firstName,
+      lastName: newUser.lastName,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin
     })
-  } else {
+  } catch(error){
     res.status(400)
-    res.json("Invalid user data")
+    throw new Error("Invalid user data")
   }
 })
 
-const loginUser = asyncHandler( async(req, res) => {
-  const { email, password } = req.body
-
-  const user = await User.findOne({email})
-  console.log(user)
-  
-  if(user && (await bcrypt.compareSync(password, user.password))){
-    res.json({
-      _id: user.id,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: auth.createAccessToken(user)
-    })
-  } else {
-    res.status(400)
-    throw new Error("Invalid Credential")
-  }
-})
-
-module.exports = {
-  registerUser,
-  loginUser
+export {
+  registerUser
 }
