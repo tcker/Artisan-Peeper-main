cdf3c4e8cffd2e6ccb3523c801b9bb9e9e0bb59b
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2024-04-03 13:17:50 +0800

    refactor: convert route import to ES module syntax
    replace the commonJS require syntax with ES module import syntax for the userRoute.js file

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
 import connectDB from "./db.js"
+import userRoute from "./routes/userRoute.js"
 
 dotenv.config()
 const port = process.env.PORT || 4000
@@ -15,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.use("/users", require("./routes/userRoute"))
+app.use("/users", userRoute)
 
 app.listen(port, () => console.log(`API is now online on port ${port}`))
 
