0cb83c7ea9f0f3bf6eda92d063a09a837a980883
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-04-05 03:27:46 +0800

    Add an admin dashboard

--- front-end/src/pages/Admin/AdminDashboard.jsx
+++ front-end/src/pages/Admin/AdminDashboard.jsx
@@ -7,29 +7,34 @@ import Top from "@/components/Top.jsx";
 function AdminDashboard() {
   return (
     <Container>
-      <div className="flex justify-between mx-4 flex-wrap ">
-        <h1 className="font-bold text-2xl">Welcome back, Admin.</h1>
-      </div>
-      <div className="flex flex-wrap gap-6 px-2 my-3">
-        <Total title="Total Applicants" total="163" added="+35"/>
-        <Total title="CV Passing Applicants" total="106" added="+15"/>
-        <Total title="Passed Assessment" total="56" added="+27"/>
-        <Total title="Total Passing" total="23" added="+3"/>
-      </div>
-      <div className="flex justify-center gap-7 flex-wrap my-7 mx-5">
-        <div className="p-5 border-2 rounded-lg shadow-md">
-          <p className="text-xl font-bold mb-5">Passed Curriculum Vitae's</p>
-          <TablePassing/>
+      <div className="flex justify-center items-center flex-col py-2">
+        <div className="flex justify-between mx-4 flex-wrap ">
+          <h1 className="font-bold text-2xl upp">Welcome back, Admin.</h1>
         </div>
-        <div className="p-5 border-2 rounded-lg shadow-md">
-          <p className="text-xl font-bold mb-3">Passed Assessments</p>
-          <Top/>
+        <div className="flex flex-wrap gap-6 px-2 my-3">
+          <Total title="Total Applicants" total="163" added="+" />
+          <Total title="CV Passing Applicants" total="106" added="+" />
+          <Total title="Passed Assessment" total="56" added="+" />
+          <Total title="Total Passing" total="23" added="+" />
         </div>
-      </div>
-
-      <div className="flex flex-col items-center justify-center mt-7 py-10 px-1 mx-5 border-2 shadow-lg">
-      <h1 className="block pl-5 mb-5 font-bold text-4xl">Applicants view</h1>
-        <JobListing/>
+        <div className="flex justify-center gap-7 flex-wrap my-7 mx-5">
+          <div className="p-5 border-2 rounded-lg shadow-md">
+            <p className="text-xl font-bold mb-5">
+              Passed Curriculum Vitae&apos;s
+            </p>
+            <TablePassing />
+          </div>
+          <div className="p-5 border-2 rounded-lg shadow-md">
+            <p className="text-xl font-bold mb-3">Passed Assessments</p>
+            <Top />
+          </div>
+        </div>
+        <div className="flex justify-center items-center flex-col mt-3">
+          <h1 className="block pl-5 mb-5 font-bold text-4xl">
+            Applicants view
+          </h1>
+          <JobListing />
+        </div>
       </div>
     </Container>
   );
