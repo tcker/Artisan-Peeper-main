63c14cac08e29e62217b8a0afeb9076f414f3380
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-04-05 00:44:45 +0800

    Resize the width of the container

--- front-end/src/components/Camera.jsx
+++ front-end/src/components/Camera.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Camera = () => {
   return (
-    <div className='flex h-48 mx-5 sm:full md:mx-0 lg:w-72 2xl:w-96 bg-black rounded-xl text-whiteborder-[1px] border-gray dark:border-white items-end'></div>
+    <div className='flex h-48 mx-5 sm:full md:mx-0 lg:w-72 2xl:w-96 bg-black rounded-xl text-whiteborder-[1px] border-gray dark:border-white dark:bg-slate-500 items-end'></div>
   )
 }
 
